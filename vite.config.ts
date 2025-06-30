84379f8696a6537fb61ce8628560e8eb59ea4cc8
DanilTsapok <daniltsapok12012004@gmail.com>
2025-06-30 16:08:02 +0300

    bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Tako",
+  base: "/Tako/",
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "src/app"),
